17617c3d10a4e35f67a13e680b926d6d6ec54770
Vojtěch Schreiner <schreinervojtech@gmail.com>
2021-10-25 23:19:41 +0200

    25.10.21_23:19

--- DU1.py
+++ DU1.py
@@ -1,35 +1,79 @@
-from turtle import forward, goto, left, pen, pendown, penup, right, back, exitonclick, speed
+from turtle import backward, forward, goto, left, pen, pendown, penup, right, back, exitonclick, speed
 import turtle
 
-turtle.speed(1)
+#nastavení herní plochy
+zi=input("Jak velkou chcete stranu herního pole (max 10)?")
+z=int(zi)
+while z<=0 or z>=10:
+    zi=input("Vložte pouze číslo od 1 do 10:")
+    z=int(zi)
+zs=str(z)
 
-for _o in range(3):
-    for _i in range(3):
+#posun želvy na začátek
+turtle.speed(0)
+penup()
+goto(-250,250)
+pendown()
+
+
+#vykreslení herní plochy
+for _o in range(z):
+    for _i in range(z):
         for _p in range(4):
             forward(50)
-            left(90)
+            right(90)
         forward(50)
-    left(90)
+    right(90)
     forward(50)
-    left(90)
-    forward(150)
-    left(180)
+    right(90)
+    forward(50*z)
+    right(180)
 
-#xi=input("Vložte x")
-#x=int(xi)
+#křížek
+print("Hráč 1 je na tahu:")
 x=11111
 y=11111
-
-while x!=1 and x!=2 and x!=3:
-    print("Vložte pouze číslo 1, 2 nebo 3")
+while x<=0 or x>=z+1:
+    print("Vložte pouze číslo od 1 do "+zs)
     xi=input("Vložte sořadnici x:")
     x=int(xi)
 
-while x!=1 and x!=2 and x!=3:
-    print("Vložte pouze číslo 1, 2 nebo 3")   
-    yi=input("Vložte souřadnici y")
+while y<=0 or y>=z+1:
+    print("Vložte pouze číslo od 1 do "+zs)   
+    yi=input("Vložte souřadnici y:")
     y=int(yi)
 
+penup()
+goto(-225+50*(x-1),225-50*(y-1))
+pendown()
+
+left(45)
+for _i in range (4):
+    forward(20)
+    backward(20)
+    left(90)
+
+#kolečko
+print("Hráč 2 je na tahu:")
+x=11111
+y=11111
+while x<=0 or x>=z+1:
+    print("Vložte pouze číslo od 1 do "+zs)
+    xi=input("Vložte sořadnici x:")
+    x=int(xi)
+
+while y<=0 or y>=z+1:
+    print("Vložte pouze číslo od 1 do "+zs)   
+    yi=input("Vložte souřadnici y:")
+    y=int(yi)
+
+penup()
+goto(-215+50*(x-1),215-50*(y-1))
+pendown()
+
+for _i in range (40):
+    forward(2)
+    left(9)
 
 
 
