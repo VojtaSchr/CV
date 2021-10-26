e6d7eb824134737243bad1013e4117f2f507e870
Vojtěch Schreiner <schreinervojtech@gmail.com>
2021-10-26 10:39:48 +0200

    26.10.21_10:38

--- DU1.py
+++ DU1.py
@@ -4,8 +4,8 @@ import turtle
 #nastavení herní plochy
 zi=input("Jak velkou chcete stranu herního pole (max 10)?")
 z=int(zi)
-while z<=0 or z>=10:
-    zi=input("Vložte pouze číslo od 1 do 10:")
+while z<=1 or z>=11:
+    zi=input("Vložte pouze číslo od 2 do 10:")
     z=int(zi)
 zs=str(z)
 
@@ -29,52 +29,57 @@ for _o in range(z):
     forward(50*z)
     right(180)
 
-#křížek
-print("Hráč 1 je na tahu:")
-x=11111
-y=11111
-while x<=0 or x>=z+1:
-    print("Vložte pouze číslo od 1 do "+zs)
-    xi=input("Vložte sořadnici x:")
-    x=int(xi)
+zz=int((z*z)/2)    
 
-while y<=0 or y>=z+1:
-    print("Vložte pouze číslo od 1 do "+zs)   
-    yi=input("Vložte souřadnici y:")
-    y=int(yi)
+for _i in range (zz):
+    #křížek
+    print("Hráč 1 je na tahu:")
+    x=11111
+    y=11111
+    while x<=0 or x>=z+1:
+        print("Vložte pouze číslo od 1 do "+zs)
+        xi=input("Vložte sořadnici x:")
+        x=int(xi)
 
-penup()
-goto(-225+50*(x-1),225-50*(y-1))
-pendown()
+    while y<=0 or y>=z+1:
+        print("Vložte pouze číslo od 1 do "+zs)   
+        yi=input("Vložte souřadnici y:")
+        y=int(yi)
 
-left(45)
-for _i in range (4):
-    forward(20)
-    backward(20)
-    left(90)
+    penup()
+    goto(-225+50*(x-1),225-50*(y-1))
+    pendown()
 
-#kolečko
-print("Hráč 2 je na tahu:")
-x=11111
-y=11111
-while x<=0 or x>=z+1:
-    print("Vložte pouze číslo od 1 do "+zs)
-    xi=input("Vložte sořadnici x:")
-    x=int(xi)
-
-while y<=0 or y>=z+1:
-    print("Vložte pouze číslo od 1 do "+zs)   
-    yi=input("Vložte souřadnici y:")
-    y=int(yi)
-
-penup()
-goto(-215+50*(x-1),215-50*(y-1))
-pendown()
-
-for _i in range (40):
-    forward(2)
-    left(9)
+    left(45)
+    for _i in range (4):
+        forward(20)
+        backward(20)
+        left(90)
+    right(45)
 
 
+    #kolečko
+    print("Hráč 2 je na tahu:")
+    x=11111
+    y=11111
+    while x<=0 or x>=z+1:
+        print("Vložte pouze číslo od 1 do "+zs)
+        xi=input("Vložte sořadnici x:")
+        x=int(xi)
+
+    while y<=0 or y>=z+1:
+        print("Vložte pouze číslo od 1 do "+zs)   
+        yi=input("Vložte souřadnici y:")
+        y=int(yi)
+
+    penup()
+    goto(-225+50*(x-1),210-50*(y-1))
+    pendown()
+
+    for _i in range (40):
+        forward(2)
+        left(9)
+
+print("Konec hry, hrací plocha zaplněna.")
 
 exitonclick()
