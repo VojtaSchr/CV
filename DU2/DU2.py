159ec2cd65a920cecd1761624160326c2dc35dcf
Vojtěch Schreiner <schreinervojtech@gmail.com>
2021-11-27 15:25:28 +0100

    27.11.21_15:25

--- DU2/DU2.py
+++ DU2/DU2.py
@@ -1,13 +1,13 @@
 import csv
 from os import execlp, write
 
-def Kontrola(in_csv_k):
+def Kontrola(in_csv_k): #kontroluje výskyt chyb ve vstupním souboru
     try:
-        with open(in_csv_k, encoding= "utf-8") as csvinfile:
+        with open(in_csv_k, encoding= "utf-8") as csvinfile:    #otevře vstupní soubor
             reader = csv.reader(csvinfile, delimiter = ",")
-            it=0
-            for row in reader:
-                it=it+1
+            it=0                                                #proměnná počítající řádky pro ValueError
+            for row in reader:                                  #cyklus s počtem opakování rovným počtu řádků v csv souboru
+                it=it+1                                         #hodnota proměnné počítající řádky se zvýší jedna 
                 try:
                     test1=float(row[5])
                 except ValueError:
@@ -24,17 +24,16 @@ def AvgW(in_csv_W):
         iw=0
         ow=0
         jeden_tyden = float(0)
-        with open("AvgW.csv",'w',newline="", encoding= "utf-8") as AvgW_Vystup:
+        with open("vystup_7dni.csv",'w',newline="", encoding= "utf-8") as AvgW_Vystup:
             csv.register_dialect("dialect1", delimiter=",")
             writer = csv.writer(AvgW_Vystup, dialect="dialect1")
             for row in reader:
-                #print(row[5])
                 iw=iw+1
                 jeden_tyden += float(row[5])
                 if iw%7==1:
                     datumW = ([row[0]] + [row[1]] + [row[2]] + [row[3]] + [row [4]])   
                 if iw%7==0:
-                    prumer_tyden = round(jeden_tyden/7, 3)
+                    prumer_tyden = round(jeden_tyden/7, 4)
                     jeden_tyden = 0
                     ow=ow+1
                     print(prumer_tyden)
@@ -50,15 +49,14 @@ def AvgY(in_csv_Y):
         iy=0
         oy=0
         jeden_rok = float(0)
-        with open("AvgY.csv",'w',newline="", encoding= "utf-8") as AvgY_Vystup:
+        with open("vystup_rok.csv",'w',newline="", encoding= "utf-8") as AvgY_Vystup:
             csv.register_dialect("dialect1", delimiter=",")
             writer = csv.writer(AvgY_Vystup, dialect="dialect1")
             for row in reader:
-                #print(row[5])
                 iy=iy+1
                 jeden_rok += float(row[5])       
                 if iy%365==0:
-                    prumer_rok = round(jeden_rok/365, 3)
+                    prumer_rok = round(jeden_rok/365, 4)
                     datumY = ([row[0]] + [row[1]] + [row[2]])
                     jeden_rok = 0
                     oy=oy+1
@@ -68,6 +66,6 @@ def AvgY(in_csv_Y):
             print(oy)
     return()
 
-Kontrola("QD_082700_Data.csv")
-AvgW("QD_082700_Data.csv")
-AvgY("QD_082700_Data.csv")
+Kontrola("vstup.csv")
+AvgW("vstup.csv")
+AvgY("vstup.csv")
