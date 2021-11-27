49aef75c7c4334660aac02063d3fcf6008c670c7
Vojtěch Schreiner <schreinervojtech@gmail.com>
2021-11-27 12:49:10 +0100

    27.11.21_12:49

--- DU2/DU2.py
+++ DU2/DU2.py
@@ -1,56 +1,8 @@
 import csv
 from os import execlp, write
-
-def AvgW():
-    with open(soubo, encoding= "utf-8") as csvinfile:
-        reader = csv.reader(csvinfile, delimiter = ",")
-        iw=0
-        ow=0
-        jeden_tyden = float(0)
-        with open("AvgW.csv",'w', encoding= "utf-8") as AvgW_Vystup:
-            csv.register_dialect("dialect1", delimiter="/")
-            writer = csv.writer(AvgW_Vystup, dialect="dialect1")
-            for row in reader:
-                #print(row[5])
-                iw=iw+1
-                jeden_tyden += float(row[5])
-                if iw%7==1:
-                    datum = (row[2] + row[3] + row [4])
-                    writer.writerow(datum)   
-                if iw%7==0:
-                    prumer_tyden = round(jeden_tyden/7, 3)
-                    jeden_tyden = 0
-                    ow=ow+1
-                    print(prumer_tyden)
-                    #writer.writerow(str(datum))
-                    writer.writerow(str(prumer_tyden))
-            print(iw)
-            print(ow)
-    return()
-
-def AvgY():
-    with open("QD_082700_Data.csv", encoding= "utf-8") as csvinfile:
-        reader = csv.reader(csvinfile, delimiter = ",")
-        iy=0
-        oy=0
-        jeden_rok = float(0)
-        for row in reader:
-            #print(row[5])
-            iy=iy+1
-            jeden_rok += float(row[5])
-            #if i%7==1:    
-            if iy%365==0:
-                prumer_rok = round(jeden_rok/365, 3)
-                jeden_rok = 0
-                oy=oy+1
-                print(prumer_rok)
-        print(iy)
-        print(oy)
-    return()
-
-def kontrola():
+def kontrola(in_csv_k):
     try:
-        with open("test_soubor.csv", encoding= "utf-8") as csvinfile:
+        with open(in_csv_k, encoding= "utf-8") as csvinfile:
             reader = csv.reader(csvinfile, delimiter = ",")
             it=0
             for row in reader:
@@ -63,8 +15,59 @@ def kontrola():
         print("Program nemá oprávnění číst vstupní soubor")
     except FileNotFoundError:
         print("Program nemůže najít vstupní soubor, zkontrolujte, že jste zadali jeho jméno (případně cestu k němu) správně")
-
     return()
 
-AvgW()
-#AvgY()
+def AvgW(in_csv_W):
+    with open(in_csv_W, encoding= "utf-8") as csvinfile:
+        reader = csv.reader(csvinfile, delimiter = ",")
+        iw=0
+        ow=0
+        jeden_tyden = float(0)
+        with open("AvgW.csv",'w', encoding= "utf-8") as AvgW_Vystup:
+            csv.register_dialect("dialect1", delimiter="/")
+            writer = csv.writer(AvgW_Vystup, dialect="dialect1")
+            for row in reader:
+                #print(row[5])
+                iw=iw+1
+                jeden_tyden += float(row[5])
+                if iw%7==1:
+                    datumW = (row[2] + row[3] + row [4])
+                    writer.writerow(datumW)   
+                if iw%7==0:
+                    prumer_tyden = round(jeden_tyden/7, 3)
+                    jeden_tyden = 0
+                    ow=ow+1
+                    print(prumer_tyden)
+                    #writer.writerow(str(datum))
+                    writer.writerow(str(prumer_tyden))
+            print(iw)
+            print(ow)
+    return()
+
+def AvgY(in_csv_Y):
+    with open(in_csv_Y, encoding= "utf-8") as csvinfile:
+        reader = csv.reader(csvinfile, delimiter = ",")
+        iy=0
+        oy=0
+        jeden_rok = float(0)
+        with open("AvgY.csv",'w', encoding= "utf-8") as AvgY_Vystup:
+            csv.register_dialect("dialect1", delimiter="/")
+            writer = csv.writer(AvgY_Vystup, dialect="dialect1")
+            for row in reader:
+                #print(row[5])
+                iy=iy+1
+                jeden_rok += float(row[5])       
+                if iy%365==0:
+                    prumer_rok = round(jeden_rok/365, 3)
+                    datumY = (row[2])
+                    jeden_rok = 0
+                    oy=oy+1
+                    print(prumer_rok)
+                    writer.writerow(str(datumY)+"   "+str(prumer_rok))
+            print(iy)
+            print(oy)
+    return()
+
+kontrola("QD_082700_Data.csv")
+AvgW("QD_082700_Data.csv")
+AvgY("QD_082700_Data.csv")
