a3c32f65dbbbe48608c6122092019660f37ffd8a
Vojtěch Schreiner <schreinervojtech@gmail.com>
2021-12-14 13:54:39 +0100

    14.12.2021_13:54

---
+++ Hledej pismeno.py
@@ -0,0 +1,12 @@
+alist = ["a","b", "c", "d", "e"]
+hledane_posmeno=str("")
+
+def find(alist, val. right, left):
+    
+    midH=((right-left)//2)
+    mid=alist(midH)
+    if len(alist)==1:
+        hledane_posmeno=alist[0]
+    if len(alist)==0:
+        print("Vkládaný řetězec je prázdný")
+    print(mid)
--- vločka.py
+++ vločka.py
@@ -1,7 +1,26 @@
 import turtle
 
-delka = int(input("Zvolte délku: "))
+delka = int(input("Zvolte délku: ")) *3
 uroven = int(input("Nastavte úroveň: "))
 
 def vlocka(d,u):
-    
+    i=0
+    x=0
+    y=0
+    if u>0:
+        i+=1
+        a=d/(3*i)
+        u=u-1
+        def zub(a):
+            turtle.forward(a)
+            turtle.left(60)
+            turtle.forward(a)
+            turtle.right(120)
+            turtle.forward(a)
+            turtle.left(60)
+            turtle.forward(a)
+        
+    turtle.exitonclick()
+    return()
+
+vlocka(delka,uroven)
