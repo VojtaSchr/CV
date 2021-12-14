b7e6f07a1ae71e70f2591654faeaf3e28bd0cb04
Vojtěch Schreiner <schreinervojtech@gmail.com>
2021-12-14 13:53:59 +0100

    Převod souřadnic, typy kontejnerů

--- DU3/kod.py
+++ DU3/kod.py
@@ -1,12 +1,46 @@
 import json
+from pyproj import Transformer
+
 
 def nacti_data(adresy, kontejnery):
-    with open(adresy, encoding="utf-8") as adresa:
-        address = json.load(adresa)
-    with open(kontejnery, encoding="utf-8") as kontejner:
-        bins = json.load(kontejner)
-    #with open("test.geojson","w", encoding="utf-8") as testS:
-        #json.dump(address, testS, indent=4)
-    return(address,bins)
+    try:
+        with open(adresy, encoding="utf-8") as adresa:
+            address_l = json.load(adresa)
+            address = address_l["features"]
+    except:
+        print("Nepodařilo se načíst data adres.")
+    try:
+        with open(kontejnery, encoding="utf-8") as kontejner:
+            bins_l = json.load(kontejner)
+            bins = bins_l["features"]
+        #with open("test.geojson","w", encoding="utf-8") as testS:
+            #json.dump(address, testS, indent=4)
+    except:
+        print("Nepodařilo se načíst data kontejnerů.")
+    return(address, bins)
+
+def WGS_to_JTSK(x,y):
+    wgs = Transformer.from_crs(4326, 5514, always_xy = True)
+    jtsk = wgs.transform(x,y)
+    print(jtsk)
+    return(jtsk)
+
+def typy_kontejneru(bins):
+    verejne_kontejnery = []
+    privatni_kontejnery = []
+    
+    for i in range (len(bins)):  
+        bins_i = bins[i]
+        if bins_i["properties"]["PRISTUP"] == "volně": 
+            verejne_kontejnery.append(bins_i)
+
+        elif bins_i["properties"]["PRISTUP"] == "obyvatelům domu":
+            privatni_kontejnery.append(bins_i)
+        
+        else:
+            print("V datech je chyba v určení přístupu k kontejnerům, program uznává pouze typy: volně nebo obyvatelům domu.")
+    return(verejne_kontejnery,privatni_kontejnery)
+
 
 nacti_data("adresy.geojson", "kontejnery.geojson")
+WGS_to_JTSK(0,0)
